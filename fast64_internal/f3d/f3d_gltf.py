2aae8af4c0087aa3bb21f573a57e0b0c2eaf5be1
Lila <lilalovesall@gmail.com>
2024-06-29 19:51:07 +0100

    attempt

--- fast64_internal/f3d/f3d_gltf.py
+++ fast64_internal/f3d/f3d_gltf.py
@@ -3,7 +3,7 @@ from bpy.types import Image
 
 from ..gltf_utility import GlTF2SubExtension
 from .f3d_gbi import F3D, get_F3D_GBI
-from .f3d_material import all_combiner_uses, F3DMaterialProperty, TextureProperty
+from .f3d_material import all_combiner_uses, update_node_values_with_preset, update_preset_manual_v4, createScenePropertiesForMaterial, F3DMaterialProperty, TextureProperty, link_f3d_material_library
 
 # TODO: Check glTF addon version instead
 if bpy.app.version >= (3, 6, 0):
@@ -77,6 +77,34 @@ class Fast64Extension(GlTF2SubExtension):
 
     def post_init(self):
         self.f3d: F3D = get_F3D_GBI()
+
+        if not self.extension.importing:
+            return
+        previous_obj = bpy.context.view_layer.objects.active
+        old_material = previous_obj.active_material if previous_obj is not None else None
+
+        # change current material and make other window's shader editor is updated
+        temp_override = get_shader_editor_context()
+        temp_object = bpy.data.objects.new("temp", None)
+        bpy.context.scene.collection.objects.link(temp_object)
+        bpy.context.view_layer.objects.active = temp_object
+
+        link_f3d_material_library()
+        f3d_mat = bpy.data.materials["fast64_f3d_material_library_beefwashere"]
+        bpy.context.object.active_material = f3d_mat
+        temp_override['space'].node_tree = f3d_mat.node_tree
+        # select all nodes and copy them to clipboard
+        for node in f3d_mat.node_tree.nodes:
+            node.select = True
+        bpy.ops.node.clipboard_copy(temp_override)
+        bpy.data.objects.remove(temp_object)
+        bpy.data.materials.remove(f3d_mat)
+
+        # back to original material
+        if previous_obj is not None:
+            bpy.context.view_layer.objects.active = previous_obj
+        if old_material is not None:
+            bpy.context.object.active_material = old_material
 
     def sampler_from_f3d(self, f3d_mat: F3DMaterialProperty, f3d_tex: TextureProperty):
         wrap = []
@@ -197,6 +225,18 @@ class Fast64Extension(GlTF2SubExtension):
         if data is None:
             return
 
+        # Copy nodes HACK
+        temp_override = get_shader_editor_context()
+        temp_override['space'].node_tree = blender_material.node_tree
+        for node in list(blender_material.node_tree.nodes):
+            blender_material.node_tree.nodes.remove(node)
+        bpy.ops.node.clipboard_paste(temp_override)
+
+        createScenePropertiesForMaterial(blender_material)
+        blender_material.is_f3d = True
+        blender_material.mat_ver = 5
+        blender_material.f3d_update_flag = True
+
         f3d_mat: F3DMaterialProperty = blender_material.f3d_mat
         f3d_mat.combiner_from_dict(data.get("combiner", {}))
         f3d_mat.colors_from_dict(data.get("colors", {}))
@@ -208,14 +248,21 @@ class Fast64Extension(GlTF2SubExtension):
             f3d_tex = f3d_mat.tex0 if num == "0" else f3d_mat.tex1
             self.gltf2_texture_to_f3d_texture(gltf2_texture, gltf, f3d_tex)
 
-        blender_material.is_f3d = True
-        blender_material.mat_ver = 5
+        blender_material.f3d_update_flag = False
+        f3d_mat.use_default_lighting = f3d_mat.use_default_lighting
 
-        # TODO: Figure out a workaround for the nodes, this will fail for now
-        # createScenePropertiesForMaterial(blender_material)
 
     def gather_import_node_after_hook(self, _vnode, gltf_node, blender_object, _gltf):
         data = self.get_gltf2_extension(gltf_node)
         if data is None:
             return
         blender_object.use_f3d_culling = data.get("use_culling", True)
+
+def get_shader_editor_context():
+    for screen in bpy.data.screens:
+        for area in screen.areas:
+            if area.type == "NODE_EDITOR":
+                for space in area.spaces:
+                    if space.tree_type == "ShaderNodeTree":
+                        context_override = {"area": area, "space": space, "screen": screen}
+                        return context_override
