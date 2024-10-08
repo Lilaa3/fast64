bcd403580c6bd41b0b90b3a90b48c39a964b92f8
Lila <lilalovesall@gmail.com>
2024-10-08 12:25:42 +0100

    fix special

--- fast64_internal/sm64/sm64_collision.py
+++ fast64_internal/sm64/sm64_collision.py
@@ -146,7 +146,7 @@ class Collision:
         if len(self.specials) > 0:
             data.source += "\tCOL_SPECIAL_INIT(" + str(len(self.specials)) + "),\n"
             for special in self.specials:
-                data.source += "\t" + special.to_c(1)
+                data.source += "\t" + special.to_c(1) + ",\n"
         if len(self.water_boxes) > 0:
             data.source += "\tCOL_WATER_BOX_INIT(" + str(len(self.water_boxes)) + "),\n"
             for waterBox in self.water_boxes:
